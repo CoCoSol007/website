d3b5471bcd4dd9d6677e408407e5c02701f7bc24
CoCoSol007 <solois.corentin@gmail.com>
2024-02-04 11:39:37 +0100

    update css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
     import "../app.css";
 </script>
 
+<nav class="bg-gray-900 p-4">
+    <div class="container mx-auto flex items-center justify-between">
+        <img class="h-8 w-8" src="logo.svg" alt="icon" />
+        <a href="/" class="font-bold text-lg">CoCoSol</a>
+        <a class="text-lg" href="/login">Login</a>
+    </div>
+</nav>
+
 <slot />
