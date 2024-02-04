637f30bdeb64e6a9692da5258705cf762c581843
CoCoSol007 <solois.corentin@gmail.com>
2024-02-04 15:43:20 +0100

    remove auth + add containerfile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,6 @@
     <div class="container mx-auto flex items-center justify-between">
         <img class="h-8 w-8" src="logo.svg" alt="icon" />
         <a href="/" class="font-bold text-lg">CoCoSol</a>
-        <a class="text-lg" href="/login">Login</a>
     </div>
 </nav>
 
