021a9c862faa806514f29c97bddcaeeeffaba6d1
CoCo_Sol <solois.corentin@gmail.com>
2024-03-24 12:06:09 +0100

    add focalboard

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,52 +2,58 @@
 
 export function fetchProjects() {
     const projects = [
-        
+
         {
             "title": "Border Wars",
             "description": "Border Wars is a turn-based online strategy game made with rust and bevy.",
             "link": "https://github.com/CoCoSol007/border-wars",
             "link_img": "border-wars.png"
         },
-            {
-                "title": "Casino Dingo",
-                "description": "Casino Dingo is a 2D game developed using the Godot engine. ",
-                "link": "https://github.com/CoCoSol007/casino-dingo",
-                "link_img": "casino_dingo.png"
-            },
-            {
-                "title": "Arcade Game",
-                "description": "Arcade is a litle retro game made with python and pygame. We made this game in 2021",
-                "link": "https://github.com/CoCoSol007/arcade",
-                "link_img": "arcade.png"
-            },
-            {
-                "title": "Simple 3D renderer",
-                "description": "It's a simple 3d rederer made in python with pygame and math",
-                "link": "https://github.com/CoCoSol007/simple-3D-renderer",
-                "link_img": "renderer.png"
-            },
-            {
-                "title": "My Website",
-                "description": "This is the code of the website you a curently looking",
-                "link": "https://github.com/CoCoSol007/my-website",
-                "link_img": "logo.svg"
-            },
-        
+        {
+            "title": "Casino Dingo",
+            "description": "Casino Dingo is a 2D game developed using the Godot engine. ",
+            "link": "https://github.com/CoCoSol007/casino-dingo",
+            "link_img": "casino_dingo.png"
+        },
+        {
+            "title": "Arcade Game",
+            "description": "Arcade is a litle retro game made with python and pygame. We made this game in 2021",
+            "link": "https://github.com/CoCoSol007/arcade",
+            "link_img": "arcade.png"
+        },
+        {
+            "title": "Simple 3D renderer",
+            "description": "It's a simple 3d rederer made in python with pygame and math",
+            "link": "https://github.com/CoCoSol007/simple-3D-renderer",
+            "link_img": "renderer.png"
+        },
+        {
+            "title": "My Website",
+            "description": "This is the code of the website you a curently looking",
+            "link": "https://github.com/CoCoSol007/my-website",
+            "link_img": "logo.svg"
+        },
+
     ]
     return projects
 }
 
 export function fetchServices() {
     const projects = [
-        
-            {
-                "title": "Uptime Kuma",
-                "description": "It is a self-hosted server monitoring tool. It can be configured to send alerts.",
-                "link": "https://up.cocosol.fr",
-                "link_img": "upkuma.svg"
-            }
-        
+
+        {
+            "title": "Uptime Kuma",
+            "description": "It is a self-hosted server monitoring tool. It can be configured to send alerts.",
+            "link": "https://up.cocosol.fr",
+            "link_img": "upkuma.svg"
+        },
+        {
+            "title": "Focalboard",
+            "description": "Focalboard is an open source, multilingual, self-hosted project management tool.",
+            "link": "https://todo.cocosol.fr",
+            "link_img": "focalboard.png"
+        }
+
     ]
     return projects
 }
