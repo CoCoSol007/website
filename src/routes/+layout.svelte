e8153b5faa22ad1b5bb2068a6d4aa80aaa5b5c8c
CoCoSol <solois.corentin@gmail.com>
2024-04-20 12:18:33 +0200

    Add a forget margin on X axe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,9 +7,9 @@
   <div class="container mx-auto flex items-center justify-between">
     <img class="h-16 w-16" src={logo} alt="icon" />
     <div class="flex items-center">
-      <a href="/" class="text-gray-300 mr-4 transition ease-in-out hover:text-blue-700">Home</a>
-      <a href="/blog" class="text-gray-300 transition ease-in-out hover:text-blue-700">Blog</a>
-      <a href="https://github.com/CoCoSol007" class="text-gray-300 transition ease-in-out hover:text-blue-700">GitHub</a>
+      <a href="/" class="text-gray-300 mx-2 transition ease-in-out hover:text-blue-700">Home</a>
+      <a href="/blog" class="text-gray-300 mx-2 transition ease-in-out hover:text-blue-700">Blog</a>
+      <a href="https://github.com/CoCoSol007" class="text-gray-300 mx-2 transition ease-in-out hover:text-blue-700">GitHub</a>
     </div>
     <h1 class="font-bold text-lg">CoCoSol</h1>
   </div>
