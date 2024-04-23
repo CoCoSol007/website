3654a1ac382c7cad24f48bd4d0078899fa1c5cec
CoCo_Sol007 <solois.corentin@gmail.com>
2024-04-23 22:03:21 +0200

    Improve blog by adding a page

--- src/lib/api.js
+++ src/lib/api.js
@@ -81,38 +81,42 @@ export async function fetchArticles() {
         "A rhythm game where you forge items from the very beginning of the process.",
       date: "04/22/2024",
       content: `
-            <h1 class="text-3xl font-bold text-center mb-4">Welcome to our submission for Godot Wild Jam #68</h1>
-            <p class="text-lg text-center mb-6">This month's theme was Forge, inspiring us to create a rhythmic adventure where you craft items from start to finish. You can play our game <a href="https://edmondada.itch.io/beats-into-shapes" class="text-blue-500 hover:underline">here</a>.</p>
-        
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 justify-center">
-            <h2 class="text-2xl font-bold col-span-full text-center">Meet Our Team</h2>
-            <div class="flex flex-col items-center">
-                <h3 class="text-lg font-bold mb-1">Tipragot</h3>
-                <p class="text-gray-600">Programmer</p>
-            </div>
-            <div class="flex flex-col items-center">
-                <h3 class="text-lg font-bold mb-1">CoCo_sol</h3>
-                <p class="text-gray-600">Programmer - Art</p>
-            </div>
-            <div class="flex flex-col items-center">
-                <h3 class="text-lg font-bold mb-1">Edmond</h3>
-                <p class="text-gray-600">Art</p>
-            </div>
-        
-`,
-      isActive: true,
+      <div class="text-left">
+          <h1 class="text-3xl font-bold mb-4 text-white">Welcome to our submission for Godot Wild Jam #68</h1>
+          <p class="text-lg mb-6 text-white">This month's theme was Forge, inspiring us to create a rhythmic adventure where you craft items from start to finish. You can play our game <a href="https://edmondada.itch.io/beats-into-shapes" class="text-blue-500 hover:underline">here</a>.</p>
+      </div>
+  
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6 justify-start">
+          <h2 class="text-2xl font-bold col-span-full mb-4 text-white">Meet Our Team</h2>
+          <div class="text-white">
+              <h3 class="text-lg font-bold mb-1">Tipragot</h3>
+              <p class="text-gray-300">Programmer</p>
+          </div>
+          <div class="text-white">
+              <h3 class="text-lg font-bold mb-1">CoCo_sol</h3>
+              <p class="text-gray-300">Programmer - Art</p>
+          </div>
+          <div class="text-white">
+              <h3 class="text-lg font-bold mb-1">Edmond</h3>
+              <p class="text-gray-300">Art</p>
+          </div>
+      </div>
+  `,
+      id: "1",
     },
     {
       title: "Introducing CoCoSol",
       description: "Get to know CoCoSol, a passionate young developer.",
       date: "04/19/2024",
-      content: `<div class="mt-4 text-center">
-            <h1 class="text-3xl font-bold mb-2">Hello World!</h1>
-            <p class="text-lg mb-4">I'm CoCoSol, a 17-year-old French coder eager to learn. Passionate about development and math, I'm a student at Caousou in France, always up for new challenges.</p>
-            <p class="text-lg">To contact me or ask questions, email me at <a href="mailto:contact@cocosol.fr" class="text-blue-500 hover:underline">contact@cocosol.fr</a>.</p>
-        </div>
-        é`,
-      isActive: true, // By default, the article is not expanded
+      content: `
+    <div class="mt-4 text-left">
+        <h1 class="text-3xl font-bold mb-2 text-white">Hello World!</h1>
+        <p class="text-lg mb-4 text-white">I'm CoCoSol, a 17-year-old French coder eager to learn. Passionate about development and math, I'm a student at Caousou in France, always up for new challenges.</p>
+        <p class="text-lg text-white">To contact me or ask questions, email me at <a href="mailto:contact@cocosol.fr" class="text-blue-500 hover:underline">contact@cocosol.fr</a>.</p>
+    </div>
+`,
+
+      id: "0",
     },
   ];
 
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -7,33 +7,19 @@
     onMount(async () => {
         articles = await fetchArticles();
     });
-
-    // Fonction pour basculer l'état de l'article lorsqu'on clique sur le titre
-    function toggleArticle(index) {
-        articles[index].show = !articles[index].show;
-        // Met à jour la liste des articles pour déclencher le rendu
-        articles = [...articles];
-    }
 </script>
 
 <!-- Blog component -->
 <div class="max-w-3xl mx-auto py-8 px-4">
     {#each articles as article, index}
-    {#if article.isActive}
-        <div class="my-4 border-b border-gray-700 {article.show ? '' : 'pb-4'}">
+        <div class="my-4 border-b border-gray-700 pb-4">
             <div class="flex justify-between items-center" >
                 <div>
-                    <a href="/" class="text-lg font-semibold text-gray-200 hover:underline cursor-pointer" on:click|preventDefault={() => toggleArticle(index)}>{article.title}</a>
+                    <a href="/blog/{article.id}" class="text-lg font-semibold text-gray-200 hover:underline cursor-pointer">{article.title}</a>
                     <p class="text-sm text-gray-400">{article.description}</p>
                 </div>
                 <p class="text-sm text-gray-400">{article.date}</p>
             </div>
-            { #if article.show }
-            <div class="pb-8 pt-4 whitespace-pre-line">
-                <p class="text-gray-300">{@html article.content}</p>
-            </div>
-            {/if}
         </div>
-    {/if}
     {/each}
 </div>
