61a3deef2b0bd2a4743bba7e05a4b5af95167015
CoCo_Sol007 <solois.corentin@gmail.com>
2024-04-24 14:13:17 +0200

    Change for better articles in blog

--- src/routes/blog/beats-into-shapes/+page.svelte
+++ src/routes/blog/beats-into-shapes/+page.svelte
@@ -1,33 +1,59 @@
 <script>
-import arrow from "../../../assets/arrow.svg";
+    import arrow from "../../../assets/arrow.svg";
+    import image1 from "../../../assets/beats/1.png";
+    import image2 from "../../../assets/beats/2.png";
+    import image3 from "../../../assets/beats/3.png";
+    import image4 from "../../../assets/beats/4.png";
+    import image5 from "../../../assets/beats/5.png";
 </script>
 
-<div class="container mx-auto w-2/3 py-8 ">
+<div class="container mx-auto w-2/3 py-8">
     <div class="flex items-center gap-2 my-4">
         <a href="/blog">
-            <img src={arrow} alt="Back to Blog" class="w-12 h-12 hover:scale-105">
+            <img src={arrow} alt="Back to Blog" class="w-12 h-12 hover:scale-125 transition">
         </a>
-        <h1 class="text-5xl font-bold text-gray-100">Introducing CoCoSol</h1>
+        <h1 class="text-5xl font-bold text-gray-100">New game project: Beats into shape</h1>
     </div>
-    <p class="text-xl text-gray-300">Get to know CoCoSol, a passionate young developer.</p>
+    <p class="text-xl text-gray-300">A rhythm game where you forge items from the very beginning of the process.</p>
     <div class="text-left">
         <h1 class="text-3xl font-bold mb-4 text-white">Welcome to our submission for Godot Wild Jam #68</h1>
         <p class="text-lg mb-6 text-white">This month's theme was Forge, inspiring us to create a rhythmic adventure where you craft items from start to finish. You can play our game <a href="https://edmondada.itch.io/beats-into-shapes" class="text-blue-500 hover:underline">here</a>.</p>
     </div>
+    
+    <section class="my-8">
+        <h2 class="text-3xl font-bold mb-4 text-white">Gameplay</h2>
+        <p class="text-lg text-white">You play as a smith in your forge, making objects in rhythm with the music.</p>
+        <p class="text-lg text-white">The game contains 4 levels with each level representing every step to create a nice and flawless metallic object.</p>
+    </section>
+
+    <section class="my-8">
+        <h2 class="text-3xl font-bold mb-4 text-white">Preview</h2>
+        <div class="grid grid-cols-2 gap-4 mb-4">    
+            <img src={image1} alt="Preview 1" class="w-full h-auto">
+            <img src={image2} alt="Preview 2" class="w-full h-auto">
+            <img src={image3} alt="Preview 3" class="w-full h-auto">
+            <img src={image4} alt="Preview 4" class="w-full h-auto">
+        </div>
+        <img src={image5} alt="Preview 5" class="w-full h-auto">
+    </section>    
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6 justify-start">
         <h2 class="text-2xl font-bold col-span-full mb-4 text-white">Meet Our Team</h2>
-        <div class="text-white">
+        <div class="text-white flex flex-col items-center">
+            <img src="https://secure.gravatar.com/avatar/4beb3e2939b1a0d1ec5664b525486b36?d=identicon&s=512" alt="Tipragot" class="w-1/2 rounded-full mb-2">
             <h3 class="text-lg font-bold mb-1">Tipragot</h3>
             <p class="text-gray-300">Programmer</p>
         </div>
-        <div class="text-white">
+        <div class="text-white flex flex-col items-center">
+            <img src="https://secure.gravatar.com/avatar/a7942845be95211969be1f821d85368c?d=identicon&s=512" alt="CoCo_sol" class="w-1/2 rounded-full mb-2">
             <h3 class="text-lg font-bold mb-1">CoCo_sol</h3>
             <p class="text-gray-300">Programmer - Art</p>
         </div>
-        <div class="text-white">
+        <div class="text-white flex flex-col items-center">
+            <img src="https://secure.gravatar.com/avatar/a953fcb8402aa74a9f84e651b57b8af2?d=identicon&s=512" alt="Edmond" class="w-1/2 rounded-full mb-2">
             <h3 class="text-lg font-bold mb-1">Edmond</h3>
             <p class="text-gray-300">Art</p>
         </div>
     </div>
+
 </div>
--- src/routes/blog/introduction/+page.svelte
+++ src/routes/blog/introduction/+page.svelte
@@ -1,19 +1,26 @@
 <script>
-import arrow from "../../../assets/arrow.svg";
+  import arrow from "../../../assets/arrow.svg";
 </script>
 
-<div class="container mx-auto w-2/3 py-8 ">
+<div class="container mx-auto max-w-2xl py-8">
     <div class="flex items-center gap-2 my-4">
         <a href="/blog">
-            <img src={arrow} alt="Back to Blog" class="w-12 h-12 hover:scale-105">
+            <img src={arrow} alt="Back to Blog" class="w-12 h-12 hover:scale-125 transition">
         </a>
         <h1 class="text-5xl font-bold text-gray-100">Introducing CoCoSol</h1>
     </div>
     <p class="text-xl text-gray-300">Get to know CoCoSol, a passionate young developer.</p>
 
-    <div class="mt-8 text-gray-700">   <div class="mt-4 text-left">
-        <h1 class="text-3xl font-bold mb-2 text-white">Hello World!</h1>
-        <p class="text-lg mb-4 text-white">I'm CoCoSol, a 17-year-old French coder eager to learn. Passionate about development and math, I'm a student at Caousou in France, always up for new challenges.</p>
-        <p class="text-lg text-white">To contact me or ask questions, email me at <a href="mailto:contact@cocosol.fr" class="text-blue-500 hover:underline">contact@cocosol.fr</a>.</p>
-    </div></div>
+    <div class="mt-8 text-gray-700">
+        <div class="mt-4 text-left">
+            <h1 class="text-3xl font-bold mb-2 text-white">Hello World!</h1>
+            <p class="text-lg mb-4 text-white">I'm CoCoSol, a 17-year-old French coder eager to learn. Passionate about development and math, I'm a student at Caousou in France, always up for new challenges.</p>
+            <p class="text-lg text-white">To contact me or ask questions, email me at <a href="mailto:contact@cocosol.fr" class="text-blue-500 hover:underline">contact@cocosol.fr</a>.</p>
+            </div>
+            <div class="flex justify-center mt-4 gap-4">
+              <img src="https://raw.githubusercontent.com/cocosol007/github-stats/master/generated/languages.svg#gh-dark-mode-only" alt="" class="mt-4">
+              <img src="https://raw.githubusercontent.com/cocosol007/github-stats/master/generated/overview.svg#gh-dark-mode-only" alt="" class="mt-4">
+          
+            </div>
+    </div>
 </div>
