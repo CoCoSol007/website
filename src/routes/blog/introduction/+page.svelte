45a56b98208188f7a3d3e460f4c4f6e698209076
CoCo_Sol007 <solois.corentin@gmail.com>
2024-04-26 14:01:00 +0200

    Update responsive of first article

--- src/routes/blog/introduction/+page.svelte
+++ src/routes/blog/introduction/+page.svelte
@@ -2,10 +2,10 @@
   import arrow from "../../../assets/arrow.svg";
 </script>
 
-<div class="container mx-auto max-w-2xl py-8">
-    <div class="flex items-center gap-2 my-4">
-        <a href="/blog">
-            <img src={arrow} alt="Back to Blog" class="w-12 h-12 hover:scale-125 transition">
+<div class="container mx-auto max-w-2xl p-8">
+    <div class="flex flex-row items-center md:items-start gap-2 m-4">
+        <a href="/blog" class="md:self-start">
+            <img src={arrow} alt="Back to Blog" class="w-full h-full hover:scale-125 transition">
         </a>
         <h1 class="text-5xl font-bold text-gray-100">Introducing CoCoSol</h1>
     </div>
@@ -17,9 +17,9 @@
             <p class="text-lg mb-4 text-white">I'm CoCoSol, a 17-year-old French coder eager to learn. Passionate about development and math, I'm a student at Caousou in France, always up for new challenges.</p>
             <p class="text-lg text-white">To contact me or ask questions, email me at <a href="mailto:contact@cocosol.fr" class="text-blue-500 hover:underline">contact@cocosol.fr</a>.</p>
             </div>
-            <div class="flex justify-center mt-4 gap-4">
-              <img src="https://raw.githubusercontent.com/cocosol007/github-stats/master/generated/languages.svg#gh-dark-mode-only" alt="" class="mt-4">
-              <img src="https://raw.githubusercontent.com/cocosol007/github-stats/master/generated/overview.svg#gh-dark-mode-only" alt="" class="mt-4">
+            <div class="flex flex-col md:flex-row justify-center mt-4 md:gap-4">
+              <img src="https://raw.githubusercontent.com/cocosol007/github-stats/master/generated/languages.svg#gh-dark-mode-only" alt="" class="mt-4 md:w-1/2">
+              <img src="https://raw.githubusercontent.com/cocosol007/github-stats/master/generated/overview.svg#gh-dark-mode-only" alt="" class="mt-4 md:w-1/2">
           
             </div>
     </div>
