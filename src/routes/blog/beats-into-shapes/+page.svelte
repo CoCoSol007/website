811dea04f9ab39a84a78adf303877d0f86d4b88a
CoCoSol <solois.corentin@gmail.com>
2024-04-27 18:09:56 +0200

    Update styling blog

--- src/routes/blog/beats-into-shapes/+page.svelte
+++ src/routes/blog/beats-into-shapes/+page.svelte
@@ -41,12 +41,12 @@
     <section class="my-8">
         <h2 class="text-3xl font-bold mb-4 text-white">Preview</h2>
         <div class="grid grid-cols-1 gap-4 mb-4 md:grid-cols-2">    
-            <img src={image1} alt="Preview 1" class="w-full h-auto">
-            <img src={image2} alt="Preview 2" class="w-full h-auto">
+            <img src={image1} alt="Preview 1" class="w-full h-auto rounded-tl-xl">
+            <img src={image2} alt="Preview 2" class="w-full h-auto rounded-tr-xl">
             <img src={image3} alt="Preview 3" class="w-full h-auto">
             <img src={image4} alt="Preview 4" class="w-full h-auto">
         </div>
-        <img src={image5} alt="Preview 5" class="w-full h-auto">
+        <img src={image5} alt="Preview 5" class="w-full h-auto rounded-b-xl">
     </section>
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6 justify-start">
