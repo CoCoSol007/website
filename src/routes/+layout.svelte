e5c9d5c8468f102ce0b59c9a8d62064c141daf23
CoCoSol <solois.corentin@gmail.com>
2024-05-15 14:17:19 +0200

    Update +layout.svelte for better accessibility

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,7 +26,7 @@ import logo from "../assets/logo.svg";
     </a>
     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse mr-4">
         <a href="/" class="text-xl font-bold text-gray-100 my-auto">CoCoSol</a>
-      <button button-name="navbar-toggle" on:click={() => toggleMenu()} type="button" class="inline-flex items-center m-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
+      <button id="navbar-toggle" on:click={() => toggleMenu()} type="button" class="inline-flex items-center m-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
         </svg>
