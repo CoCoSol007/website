d531db4253edd27c2f016ed007394dabe48bc1ca
CoCoSol <solois.corentin@gmail.com>
2024-05-24 11:06:45 +0200

    Make the email an hyperlink

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -116,7 +116,7 @@ const age = Math.floor((currentDate - birthDate) / (1000 * 60 * 60 * 24 * 365));
                 Although I do not have much experience, I am passionate about development
                 and math. Currently, I am a student at Caousou in France <br /> 
                 and I am always seeking new challenges. <br /> To contact me
-                or ask any questions, please email me at contact@cocosol.fr. <br />
+                or ask any questions, please email me at <a href="mailto://contact@cocosol.fr">contact@cocosol.fr</a><br />
             </p>
         </div>
         <div class="md:w-1/2 relative">
